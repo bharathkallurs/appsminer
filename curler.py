a50b1ac77016d6fa6ee2e88ba88029f3655790e9
Bharath S Kallur <bharathkallurs@gmail.com>
2015-04-02 00:21:58 +0530

    Update curler.py
    Made it closer to PEP8 complaince

--- curler.py
+++ curler.py
@@ -1,35 +1,24 @@
 #the driver program to run the app details miner
-#will be updating as and when i write new code
 
 import os, sys
 from appMiner import mineApps
-#from config import PROD_PORT, DEFAULT_PORT, AUTH_CODE
-#from app import app as app_table
+
 
 
 def extract_app_details():
 	#give a URL from where you will fetch the details.
 	mainURL = "https://play.google.com"
-
 	#create an initial object for the page
 	pager = mineApps(mainURL)
-
 	#create a page detailer object for accessing urls present in main page
 	status , pagerObj = pager.fetch_page_details()
-
 	#fetch all the URLs listing app details present in the parent page
 	status , app_urls , see_more_urls = pager.fetch_app_url(pagerObj, True)
 
-	#fetch the urls of apps from a search.
-	#this will list the app searched for and also fetch data from all related apps
-	#just remove comment when you want this to work
-	#searchString = ["zombies", "vs", "plants"]
-	#status, app_urls = pager.fetch_related_app_details(searchString)
-
-	#url = "https://play.google.com/store/apps/details?id=com.king.candycrushsaga"
-	#status , details = pager.fetch_app_details(url)
+	url = "https://play.google.com/store/apps/details?id=com.king.candycrushsaga"
+	status , details = pager.fetch_app_details(url)
 	'''
-	#fetch the details of each object
+	#fetch the details of each object if there are multiple app urls
 	for url in app_urls : 
 		status , details = pager.fetch_app_details(url)
 		if status == 0 :
